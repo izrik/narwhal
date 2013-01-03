1f80edb1e60d973260cb5cc81c84ed1c6f84fd04
Richard Sartor <richard.sartor@rackspace.com>
2013-01-03 16:04:36 -0600

    Removed old shell scripts.

--- catalina-vars.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-export CATALINA_HOME=/usr/share/tomcat7
--- setup-tomcat/setup-tomcat.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-groupadd tomcat
-useradd -g tomcat tomcat
-
-echo tomcat | passwd -- stdin tomcat
-
-yum -y install tomcat7
-
