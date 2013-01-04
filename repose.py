4632ddebf69d7ceb1d818c4f0fb5a66d49683d9e
Richard Sartor <richard.sartor@rackspace.com>
2013-01-04 15:42:46 -0600

    Extracted the repose module.

---
+++ repose.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+import subprocess
+import socket
+
+
+_default_jar_file = 'usr/share/repose/repose-valve.jar'
+
+
+class ReposeValve:
+    def __init__(self, config_dir, port, jar_file=None, stop_port=None,
+                 insecure=False):
+
+        if jar_file is None:
+            jar_file = _default_jar_file
+
+        if stop_port is None:
+            stop_port = port + 1000
+
+        self.config_dir = config_dir
+        self.port = port
+        self.jar_file = jar_file
+        self.stop_port = stop_port
+        self.insecure = insecure
+
+        pargs = [
+            'java', '-jar', jar_file,
+            '-c', config_dir,
+            '-p', str(port),
+            '-s', str(stop_port)
+        ]
+
+        if insecure:
+            pargs.append('-k')
+
+        pargs.append('start')
+
+        self.proc = subprocess.Popen(pargs, stdout=subprocess.PIPE,
+                                     stderr=subprocess.PIPE)
+
+    def stop(self):
+        s = socket.create_connection(('localhost', self.stop_port))
+        s.send('stop\r\n')
+        if self.proc.poll() is None:
+            self.proc.communicate()
--- run_repose.py
+++ run_repose.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python
 
 import argparse
-import subprocess
 import time
-import socket
 
-_default_jar_file = 'usr/share/repose/repose-valve.jar'
+import repose
 
 
 def run():
@@ -22,58 +20,20 @@ def run():
     parser.add_argument('--insecure', help='Don\'t verify SSL certs.',
                         action='store_true')
     parser.add_argument('--jar-file', help='The Repose Valve JAR file to run.',
-                        default=_default_jar_file)
+                        default=repose._default_jar_file)
 
     args = parser.parse_args()
 
     if args.stop_port is None:
         args.stop_port = int(args.port) + 1000
 
-    r = ReposeValve(jar_file=args.jar_file, config_dir=args.config_dir,
-                    port=args.port, stop_port=args.stop_port,
-                    insecure=args.insecure)
+    r = repose.ReposeValve(jar_file=args.jar_file, config_dir=args.config_dir,
+                           port=args.port, stop_port=args.stop_port,
+                           insecure=args.insecure)
 
     time.sleep(35)
 
     r.stop()
-
-
-class ReposeValve:
-    def __init__(self, config_dir, port, jar_file=None, stop_port=None,
-                 insecure=False):
-
-        if jar_file is None:
-            jar_file = _default_jar_file
-
-        if stop_port is None:
-            stop_port = port + 1000
-
-        self.config_dir = config_dir
-        self.port = port
-        self.jar_file = jar_file
-        self.stop_port = stop_port
-        self.insecure = insecure
-
-        pargs = [
-            'java', '-jar', jar_file,
-            '-c', config_dir,
-            '-p', str(port),
-            '-s', str(stop_port)
-        ]
-
-        if insecure:
-            pargs.append('-k')
-
-        pargs.append('start')
-
-        self.proc = subprocess.Popen(pargs, stdout=subprocess.PIPE,
-                                     stderr=subprocess.PIPE)
-
-    def stop(self):
-        s = socket.create_connection(('localhost', self.stop_port))
-        s.send('stop\r\n')
-        if self.proc.poll() is None:
-            self.proc.communicate()
 
 
 if __name__ == '__main__':
