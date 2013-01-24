968acffdcffdc1775bb0dbb253d1a42335a4aaab
Richard Sartor <richard.sartor@rackspace.com>
2013-01-24 14:12:04 -0600

    Added logging.

--- repose.py
+++ repose.py
@@ -2,6 +2,10 @@
 
 import subprocess
 import socket
+import logging
+
+
+logger = logging.getLogger(__name__)
 
 
 _default_jar_file = 'usr/share/repose/repose-valve.jar'
@@ -10,6 +14,9 @@ _default_jar_file = 'usr/share/repose/repose-valve.jar'
 class ReposeValve:
     def __init__(self, config_dir, port=None, jar_file=None, stop_port=None,
                  insecure=False):
+        logger.debug('Creating new ReposeValve object (config_dir=%s, '
+                     'jar_file=%s, stop_port=%s, insecure=%s)' %
+                     (config_dir, jar_file, stop_port, insecure))
 
         if jar_file is None:
             jar_file = _default_jar_file
@@ -40,15 +47,25 @@ class ReposeValve:
 
         self.proc = subprocess.Popen(pargs, stdout=subprocess.PIPE,
                                      stderr=subprocess.PIPE)
+        logger.debug('New ReposeValve object initialized (pid=%i)' %
+                     self.proc.pid)
+
 
     def stop(self, wait=True):
         try:
+            logger.debug('Attempting to stop ReposeValve object (pid=%i, '
+                         'stop_port=%s)' % (self.proc.pid, self.stop_port))
             s = socket.create_connection(('localhost', self.stop_port))
             s.send('stop\r\n')
             if wait:
+                logger.debug('Waiting for process to end (pid=%i)' %
+                             self.proc.pid)
                 self.wait()
         except:
+            logger.debug('Couldn\'t stop using the stop port, killing instead '
+                         '(pid=%i)' % self.proc.pid)
             self.proc.kill()
+        logger.debug('Repose stopped (pid=%i)' % self.proc.pid)
 
     def wait(self):
         return self.proc.communicate()
