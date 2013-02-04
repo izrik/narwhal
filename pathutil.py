85163780b249d0a3e8df4428dd0259ef9d770191
Richard Sartor <richard.sartor@rackspace.com>
2013-02-04 13:00:45 -0600

    Added the clear_folder function.

--- pathutil.py
+++ pathutil.py
@@ -14,5 +14,14 @@ def create_folder(folder_name):
         os.makedirs(folder_name)
 
 
+def clear_folder(folder_name):
+    for _name in os.listdir(folder_name):
+        name = join(folder_name, _name)
+        if os.path.isdir(name):
+            delete_folder(name)
+        else:
+            os.remove(name)
+
+
 def join(a, *p):
     return os.path.normpath(os.path.join(a, *p))
